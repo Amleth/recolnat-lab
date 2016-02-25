793d1d7556a44f5d284eb55920a2de6308c92e5f
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2016-02-25 11:01:22 +0100

    Deploy profiles available from now on : dev (local), ddev (server), test, test-demo, test, prod

--- colaboratory/gulpfile.js
+++ colaboratory/gulpfile.js
@@ -3,6 +3,8 @@ var sftp = require('gulp-sftp');
 var shell = require('gulp-shell');
 var rename = require('gulp-rename');
 
+var distSrc = ['dist/*'];
+
 //
 // DEV
 //
@@ -15,6 +17,34 @@ gulp.task('conf-dev', function() {
   gulp.src('./conf/ApplicationConfiguration-dev.js')
     .pipe(rename('ApplicationConfiguration.js'))
     .pipe(gulp.dest('./src/conf'));
+});
+
+//
+// DEV SERVER
+//
+gulp.task('html', function() {
+  gulp.src('./build/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-deploy-dev', ['conf-deploy-dev', 'html'], shell.task([
+  'webpack -d --config webpack.production.config.js --progress --colors'
+]));
+
+gulp.task('conf-deploy-dev', function() {
+  gulp.src('./conf/ApplicationConfiguration-test.js')
+    .pipe(rename('ApplicationConfiguration.js'))
+    .pipe(gulp.dest('./src/conf'));
+});
+
+gulp.task('deploy-dev', ['build-deploy-dev'], function() {
+  gulp.src(distSrc)
+    .pipe(sftp({
+      host: 'wp5test.mnhn.fr',
+      remotePath: '/home/cnamuser/www/labo-dev',
+      user: 'cnamuser',
+      pass: ""
+    }));
 });
 
 //
@@ -36,10 +66,66 @@ gulp.task('conf-test', function() {
 });
 
 gulp.task('deploy-test', ['build-test'], function() {
-  gulp.src('dist/*')
+  gulp.src(distSrc)
     .pipe(sftp({
       host: 'wp5test.mnhn.fr',
       remotePath: '/home/cnamuser/www/labo',
+      user: 'cnamuser',
+      pass: ""
+    }));
+});
+
+//
+// TEST DEMO
+//
+gulp.task('html', function() {
+  gulp.src('./build/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-demo-test', ['conf-demo-test', 'html'], shell.task([
+  'webpack -d --config webpack.production.config.js --progress --colors'
+]));
+
+gulp.task('conf-demo-test', function() {
+  gulp.src('./conf/ApplicationConfiguration-demo-test.js')
+    .pipe(rename('ApplicationConfiguration.js'))
+    .pipe(gulp.dest('./src/conf'));
+});
+
+gulp.task('deploy-demo-test', ['build-demo-test'], function() {
+  gulp.src(distSrc)
+    .pipe(sftp({
+      host: 'wp5test.mnhn.fr',
+      remotePath: '/home/cnamuser/www/demo-testing',
+      user: 'cnamuser',
+      pass: ""
+    }));
+});
+
+//
+// DEMO
+//
+gulp.task('html', function() {
+  gulp.src('./build/index.html')
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-demo', ['conf-demo', 'html'], shell.task([
+  'webpack -p --config webpack.production.config.js --progress --colors'
+]));
+
+gulp.task('conf-demo', function() {
+  gulp.src('./conf/ApplicationConfiguration-demo.js')
+    .pipe(rename('ApplicationConfiguration.js'))
+    .pipe(gulp.dest('./src/conf'));
+});
+
+gulp.task('deploy-demo', ['build-demo'], function() {
+  gulp.src(distSrc)
+    .pipe(sftp({
+      host: 'wp5test.mnhn.fr',
+      remotePath: '/home/cnamuser/www/demo',
       user: 'cnamuser',
       pass: ""
     }));
@@ -59,7 +145,7 @@ gulp.task('conf-prod', function() {
 });
 
 gulp.task('deploy-prod', ['build-prod'], function() {
-  gulp.src('dist/*')
+  gulp.src(distSrc)
     .pipe(sftp({
       host: 'wp5prod.mnhn.fr',
       remotePath: '/home/cnam/www/labo',

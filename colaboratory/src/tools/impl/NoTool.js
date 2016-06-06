b5c2eb5681375e78ceb340b469edae2ecf5cbcab
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2016-06-06 17:00:13 +0100

    Added the no-tool tool

---
+++ colaboratory/src/tools/impl/NoTool.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React from 'react';
+
+import AbstractTool from '../AbstractTool';
+
+import ToolActions from '../../actions/ToolActions';
+
+import ToolConf from '../../conf/Tools-conf';
+
+import icon from '../../images/pointer.png';
+
+class NoTool extends AbstractTool {
+  constructor(props) {
+    super(props);
+
+    this.state = this.initialState();
+  }
+
+  initialState() {
+    return {
+      active: false
+    }
+  }
+
+  setMode() {
+    ToolActions.setTool(ToolConf.nothing.uid);
+  }
+
+  render() {
+    return (
+      <button ref='button'
+        style={this.buttonStyle}
+        className='ui button compact'
+        onClick={this.setMode}
+        data-content="Aucun outil actif.">
+        <img src={icon} style={this.iconStyle} height='20px' width='20px' />
+      </button>
+    );
+  }
+}
+
+export default NoTool;

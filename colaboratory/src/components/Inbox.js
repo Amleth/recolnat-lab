624ef594fb5ead75592eb175b883334a51515989
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2016-06-06 16:58:08 +0100

    Split automated and manual image placement into two differents tabs to avoid user confusion.

--- colaboratory/src/components/Inbox.js
+++ colaboratory/src/components/Inbox.js
@@ -157,6 +157,7 @@ class Inbox extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if(this.state.active && this.state.open) {
+      $('.menu .item', $(this.refs.tabs.getDOMNode())).tab();
       $(this.refs.image.getDOMNode()).popup();
       $('.ui.button', $(this.refs.buttons.getDOMNode())).popup();
     }
@@ -178,32 +179,51 @@ class Inbox extends React.Component {
         <div className='ui button teal' onClick={this.open.bind(this)}>Vous avez {this.state.content.length} images à placer</div>
       </div>
     }
-    return <div className='ui segment' style={this.componentStyle}>
-      <img className='ui image'
-           ref='image'
-           data-content="Faites glisser l'image vers le bureau pour la placer"
-           src={this.state.content[this.state.selected].thumbnail}
-           alt='Chargement en cours'
-           draggable='true'
-           onDragStart={this.startDragImage.bind(this)}/>
-      <div className='ui tiny compact buttons'>
-        <div className='ui button' onClick={this.previous.bind(this)}><i className='ui left chevron icon' /></div>
-        <div className='ui button disabled'>{this.state.selected+1}/{this.state.content.length}</div>
-        <div className='ui button' onClick={this.next.bind(this)}><i className='ui right chevron icon' /></div>
+    return <div className='ui segment' style={this.componentStyle} ref='tabs'>
+    <div className="ui top attached fitted tabular menu">
+      <div
+      className="active item"
+      data-tab="automatic">
+      Auto
       </div>
+      <div className="item" data-tab="manual">
+      Manuel
+      </div>
+    </div>
+    <div className="ui bottom attached active tab segment" data-tab="automatic">
+    <div className='ui button disabled'>{this.state.content.length} images</div>
+      <div className='ui tiny two buttons'
+    ref='buttons'
+    >
+      <div className='ui button'
+           onClick={this.placeAllImagesInLine.bind(this)}
+           data-content='Placer toutes les images non-affichées en ligne à partir de la vue'>
+        <i className='ui ellipsis horizontal icon' />
+      </div>
+      <div className='ui button'
+           onClick={this.placeAllImagesInColumn.bind(this)}
+           data-content='Placer toutes les images non-affichées en colonne à partir de la vue'>
+        <i className='ui ellipsis vertical icon' />
+      </div>
+    </div>
+</div>
+<div className="ui bottom attached tab segment" data-tab="manual">
+<img className='ui image'
+     ref='image'
+     data-content="Faites glisser l'image vers le bureau pour la placer"
+     src={this.state.content[this.state.selected].thumbnail}
+     alt='Chargement en cours'
+     draggable='true'
+     onDragStart={this.startDragImage.bind(this)}/>
+<div className='ui mini three buttons'>
+  <div className='ui button' onClick={this.previous.bind(this)}><i className='ui left chevron icon' /></div>
+  <div className='ui button disabled'>{this.state.selected+1}/{this.state.content.length}</div>
+  <div className='ui button' onClick={this.next.bind(this)}><i className='ui right chevron icon' /></div>
+</div>
+</div>
 
-      <div className='ui tiny compact buttons' ref='buttons'>
-        <div className='ui button'
-             onClick={this.placeAllImagesInLine.bind(this)}
-             data-content='Placer toutes les images non-affichées en ligne à partir de la vue'>
-          <i className='ui ellipsis horizontal icon' />
-        </div>
-        <div className='ui button'
-             onClick={this.placeAllImagesInColumn.bind(this)}
-             data-content='Placer toutes les images non-affichées en colonne à partir de la vue'>
-          <i className='ui ellipsis vertical icon' />
-        </div>
-      </div>
+
+
     </div>
   }
 }

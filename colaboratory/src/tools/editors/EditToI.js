75d2408141ae3df1d8b177302a8522db310dc3a5
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2016-06-14 18:36:59 +0100

    Beginning of implementation for line editor

---
+++ colaboratory/src/tools/editors/EditToI.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import d3 from 'd3';
+
+import Classes from '../../constants/CommonSVGClasses';
+
+import MetadataActions from '../../actions/MetadataActions';
+
+import REST from '../../utils/REST';
+
+class EditToI {
+
+  // static startEdit(data) {
+  //   var vertices = JSON.parse(data.polygonVertices);
+  //   // Select ToI container, append vertices for edit
+  //   var editor = d3.select(d3.select('#PATH-' + data.uid).node().parentNode)
+  //     .append('g')
+  //     .attr('class', 'TOI-EDITOR');
+  //
+  //   for(var i = 0; i < vertices.length; ++i) {
+  //     var vertex = vertices[i];
+  //     editor.append()
+  //   }
+  // }
+}
+
+export default EditToI

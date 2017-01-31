b1552b6d6c4ca95b1f5f710ea647c98570ef5782
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2017-01-31 15:53:56 +0100

    Util for placing images outside a component lifetime

--- colaboratory/src/utils/ImagePlacer.js
+++ colaboratory/src/utils/ImagePlacer.js
@@ -1,3 +1,37 @@
 /**
  * Created by dmitri on 31/01/17.
  */
+'use strict';
+
+import ServiceMethods from './ServiceMethods';
+
+class ImagePlacer {
+  constructor(viewId, images, positions, allPlacedCallback) {
+    this.viewId = viewId;
+    this.images = images;
+    this.positions = positions;
+    this.allPlacedCallback = allPlacedCallback;
+    this.placed = 0;
+  }
+
+  imagePlaced() {
+    this.placed++;
+    if(this.placed === this.images.length) {
+      this.allPlacedCallback(this.viewId, this.images, this.positions);
+    }
+  }
+
+  run() {
+    if(this.images.length !== this.positions.length) {
+      console.error('Number of images and positions must be the same. Got ' + this.images.length + ' images and ' + this.positions.length + ' positions.');
+      return;
+    }
+    for(let i = 0; i < this.images.length; ++i) {
+      let image = this.images[i];
+      let pos = this.positions[i];
+      ServiceMethods.place(this.viewId, image, pos.x, pos.y, this.imagePlaced.bind(null, this.viewId));
+    }
+  }
+}
+
+export default ImagePlacer;

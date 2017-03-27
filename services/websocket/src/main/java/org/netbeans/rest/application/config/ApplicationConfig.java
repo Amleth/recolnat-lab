26d801f34571f6e6235f49193fae9f3326ce8081
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2017-03-27 14:13:19 +0200

    Added export service

--- services/websocket/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ services/websocket/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,6 +29,8 @@ public class ApplicationConfig extends Application {
    * If required, comment out calling this method in getClasses().
    */
   private void addRestResourceClasses(Set<Class<?>> resources) {
+    resources.add(org.dicen.recolnat.services.resources.rest.ExportDownloadService.class);
+    resources.add(org.dicen.recolnat.services.resources.rest.TagService.class);
   }
   
 }
